82ad5511e135dd13d7edd64f0d7d4c54a167fd6f
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-07-08 14:33:52 -0300

    Criação do projeto usando spring boot e maven

---
+++ src/main/java/br/com/db1/gumga/passwordmeter/Application.java
@@ -0,0 +1,12 @@
+package br.com.db1.gumga.passwordmeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
